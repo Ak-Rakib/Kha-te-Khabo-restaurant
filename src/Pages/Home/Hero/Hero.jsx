a874ccbc0818870ab723b2d6758eb338fb1a7747
Ak-Rakib <rkrakibsaimon@gmail.com>
2024-01-01 21:27:59 +0600

    set banner

--- src/Pages/Home/Hero/Hero.jsx
+++ src/Pages/Home/Hero/Hero.jsx
@@ -7,17 +7,17 @@ const Hero = () => {
       <div
         className="hero min-h-screen mb-10"
         style={{
-          backgroundImage: "url(https://ibb.co/PhzH98L)",
+          backgroundImage: "url(https://i.ibb.co/zRHv7wM/pexels-waldemar-2290070.jpg)",
         }}
       >
         <div className="hero-overlay bg-opacity-60"></div>
         <div className="hero-content text-center text-neutral-content">
           <div className="max-w-md">
-            <h1 className="mb-5 text-5xl font-bold">Kha-te-Khabo</h1>
-            <p className="mb-5">
+            <h1 className="mb-5 text-5xl font-bold font-light font-mono ">Kha-te-Khabo</h1>
+            <p className="mb-5 font-light">
               The Best Restaurant available now in Chittagong
             </p>
-            <p className="mb-5">
+            <p className="mb-5 font-light">
               Book online or call{" "}
               <span className="mbl-num">+88017843*****</span>
             </p>
