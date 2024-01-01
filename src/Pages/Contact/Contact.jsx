628ad44055b35083378959de68bac63dd2b325b5
Ak-Rakib <rkrakibsaimon@gmail.com>
2024-01-01 20:59:27 +0600

    modify navbar and home banner

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React from "react";
 
 const Contact = () => {
-    return (
-        <div>
-            This is Contact Page
-        </div>
-    );
+  return (
+    <div>
+      <button className="btn">Button</button>
+      <button className="btn btn-neutral">Neutral</button>
+      <button className="btn btn-primary">Primary</button>
+      <button className="btn btn-secondary">Secondary</button>
+      <button className="btn btn-accent">Accent</button>
+      <button className="btn btn-ghost">Ghost</button>
+      <button className="btn btn-link">Link</button>
+    </div>
+  );
 };
 
 export default Contact;
--- src/Pages/Home/Hero/Hero.jsx
+++ src/Pages/Home/Hero/Hero.jsx
@@ -1,22 +1,32 @@
-import React from 'react';
-import './Hero.css'
+import React from "react";
+
 
 const Hero = () => {
-    return (
-        <div className='hero-img container'>
-            <div className="hero-content">
-                <h1 className="heading-primary">
-                    <span>Welcome</span> To <span className='restaurant'>Kha-te-Khabo</span>
-                </h1>
-                <p className='text-white'>
-                    The Best Restaurant  available now in Chittagong
-                </p>
-                <p className='text-white'>
-                    Book online or call <span className='mbl-num'>+88017843*****</span>
-                </p>
-            </div>
+  return (
+    <>
+      <div
+        className="hero min-h-screen mb-10"
+        style={{
+          backgroundImage: "url(https://ibb.co/PhzH98L)",
+        }}
+      >
+        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="hero-content text-center text-neutral-content">
+          <div className="max-w-md">
+            <h1 className="mb-5 text-5xl font-bold">Kha-te-Khabo</h1>
+            <p className="mb-5">
+              The Best Restaurant available now in Chittagong
+            </p>
+            <p className="mb-5">
+              Book online or call{" "}
+              <span className="mbl-num">+88017843*****</span>
+            </p>
+            <button className="btn btn-primary">Get Started</button>
+          </div>
         </div>
-    );
+      </div>
+    </>
+  );
 };
 
 export default Hero;
