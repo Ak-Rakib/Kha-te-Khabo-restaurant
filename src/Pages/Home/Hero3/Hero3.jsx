c6f1d4de0c498dd84894c630a33233f6c6be15ba
Ak-Rakib <rkrakibsaimon@gmail.com>
2024-01-03 13:31:11 +0600

    set second section

---
+++ src/Pages/Home/Hero3/Hero3.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Hero3 = () => {
+  return (
+    <div className="flex-none md:flex gap-4 place-content-evenly mt-3">
+      <div className="content">
+        <p className="text-yellow-500 text-2xl">Book a Table</p>
+        <h1 className="text-3xl">Book Your Table For Private Dinners & Happy Hours</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum
+          repellat provident, consectetur aperiam voluptas nihil officia ducimus
+          reprehenderit labore magni quasi delectus, aliquid, nesciunt incidunt
+          pariatur sed ab laudantium iste?
+        </p>
+      </div>
+      <div className="form">
+        <p>Hello</p>
+      </div>
+    </div>
+  );
+};
+
+export default Hero3;
